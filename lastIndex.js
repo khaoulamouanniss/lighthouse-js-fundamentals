1771ba0e6a135ce753a1439feb49c213bebf6715
Khaoula <khaoula.mouanniss@gmail.com>
2020-11-12 13:45:04 +0000

    find the last index of a value in an array

---
+++ lastIndex.js
@@ -0,0 +1,15 @@
+const lastIndexOf = function (arr,val){
+  let ind = -1;
+  for (let i = 0 ; i < arr.length ; i ++){
+    if (arr[i] === val){
+      ind = i;
+    }
+  }
+  return ind;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
